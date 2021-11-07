77a278eaaa0320a10ab28a3ae59504c980f2bc3b
Jennifer Stark <jastark1@gmail.com>
2021-11-07 17:47:59 +0000

    fix additional tf syntax issues not clear in tf docs

--- environments/dev/flask_app.tf
+++ environments/dev/flask_app.tf
@@ -3,11 +3,13 @@ resource "google_cloudbuild_trigger" "dev-flask-app-filename-trigger" {
   description     = "PROD Cloud build trigger to rebuild Docker container for cloud run"
   github {
     name          = "gcp-cloud-run-flask-app"
-    push          = ".*"
+    push {
+      branch      = ".*"
+      }
   }
 
   filename        = "./flask_app_cloud_run/Dockerfile"
-  included_files  = "./flask_app_cloud_run/*"
+  included_files  = ["./flask_app_cloud_run/Dockerfile", "./flask_app_cloud_run/requirements.txt", "./flask_app_cloud_run/main.py",]
 
   build {
     images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/gcp-cloud-run-flask-app-test/flask-test-image:tag1"]
@@ -15,7 +17,7 @@ resource "google_cloudbuild_trigger" "dev-flask-app-filename-trigger" {
       name        = "gcr.io/cloud-builders/docker"
       args        = ["build", "t", "europe-west1-docker.pkg.dev/$PROJECT_ID/gcp-cloud-run-flask-app-test/flask-test-image:tag1", "."]
       id          = "build docker image for flask_app_cloud_run"
-      wait_for    = "tf plan"
+      wait_for    = ["tf plan"]
     }
   }
 }
