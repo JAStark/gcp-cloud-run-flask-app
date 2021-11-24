89c62fcf279d693f7e84bebdeec2c7c7a5648d71
Jennifer Stark <jastark1@gmail.com>
2021-11-24 18:24:10 +0000

    add provider to artifact registry resource

--- environments/dev/terraform.tf
+++ environments/dev/terraform.tf
@@ -19,10 +19,11 @@ provider "google" {
 }
 
 resource "google_artifact_registry_repository" "flask-app-cloud-run-repo"     {
-  provider = google-beta
+  provider      = google-beta
 
-  location = "europe-west1"
+  project       = "silver-antonym-326607"
+  location      = "europe-west1"
   repository_id = "gcp-cloud-run-flask-app-test"
-  description = "tf-created repo for flask with endpoints example"
-  format = "DOCKER"
+  description   = "tf-created repo for flask with endpoints example"
+  format        = "DOCKER"
 }
