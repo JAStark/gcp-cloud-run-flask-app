20f4e898437a40d13fd348e600d5f39bf75193c3
Jennifer Stark <jastark1@gmail.com>
2021-11-24 19:52:25 +0000

    comment out run; add in $SHORT_SHA

--- environments/prod/flask_app.tf
+++ environments/prod/flask_app.tf
@@ -13,10 +13,10 @@ resource "google_cloudbuild_trigger" "prod-flask-app-filename-trigger" {
   included_files  = ["./flask_app_cloud_run/Dockerfile", "./flask_app_cloud_run/requirements.txt", "./flask_app_cloud_run/main.py",]
 
   build {
-    images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-test/flask-endpoint-image:tag_1"]
+    images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-test/flask-endpoint-image:$SHORT_SHA"]
     step {
       name        = "gcr.io/cloud-builders/docker"
-      args        = ["build", "t", "europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-test/flask-endpoint-image:tag_1", "./flask_app_cloud_run/Dockerfile"]
+      args        = ["build", "t", "europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-test/flask-endpoint-image:$SHORT_SHA", "./flask_app_cloud_run/Dockerfile"]
       id          = "build docker image for flask_app_cloud_run"
       wait_for    = ["tf plan"]
     }
@@ -24,20 +24,20 @@ resource "google_cloudbuild_trigger" "prod-flask-app-filename-trigger" {
 }
 
 
-resource "google_cloud_run_service" "prod-flask-app-cloud-run" {
-  name      = "prod-flask-app-cloud-run-service"
-  location  = "europe-west1"
-
-  template {
-    spec {
-      containers {
-        image = "europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-example/flask-endpoint-image:tag_1"
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+# resource "google_cloud_run_service" "prod-flask-app-cloud-run" {
+#   name      = "prod-flask-app-cloud-run-service"
+#   location  = "europe-west1"
+#
+#   template {
+#     spec {
+#       containers {
+#         image = "europe-west1-docker.pkg.dev/$PROJECT_ID/prod-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA"
+#       }
+#     }
+#   }
+#
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
