709e4dbd2448e4a36649230eeaca4704b3f7fb07
Jennifer Stark <jastark1@gmail.com>
2021-11-25 20:01:05 +0000

    back to `$SHORT_SHA` ... not sure why `run` cant find it

--- environments/dev/flask_app.tf
+++ environments/dev/flask_app.tf
@@ -12,10 +12,10 @@ resource "google_cloudbuild_trigger" "dev-flask-app-build-trigger" {
   included_files  = ["./flask_app_cloud_run/Dockerfile", "./flask_app_cloud_run/requirements.txt", "./flask_app_cloud_run/main.py",]
 
   build {
-    images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$COMMIT_SHA"]
+    images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA"]
     step {
       name        = "gcr.io/cloud-builders/docker"
-      args        = ["build", "-f", "./flask_app_cloud_run/Dockerfile", "-t", "europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$COMMIT_SHA", "./flask_app_cloud_run" ]
+      args        = ["build", "-f", "./flask_app_cloud_run/Dockerfile", "-t", "europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA", "./flask_app_cloud_run" ]
       id          = "build & push flask_app_cloud_run image"
     }
   }
@@ -29,7 +29,7 @@ resource "google_cloud_run_service" "dev-flask-app-cloud-run" {
   template {
     spec {
       containers {
-        image = "europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$COMMIT_SHA"
+        image = "europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA"
       }
     }
   }
@@ -39,5 +39,5 @@ resource "google_cloud_run_service" "dev-flask-app-cloud-run" {
     latest_revision = true
   }
 
-  depends_on = [google_cloudbuild_trigger.dev-flask-app-build-trigger]
+  depends_on = [google_cloudbuild_trigger.dev-flask-app-build-trigger.name]
 }
