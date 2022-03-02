3551b6051569eb6f53e5ae6e31353ef4ec7f9d76
Jennifer Stark <jastark1@gmail.com>
2022-03-02 08:53:05 +0000

    placeholder commit (computer dying)

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # gcp-cloud-run-flask-app
 Following blog post https://medium.com/fullstackai/how-to-deploy-a-simple-flask-app-on-cloud-run-with-cloud-endpoint-e10088170eb7 by Takashi Nakamura to deploy a python Flask app with Endpoint.
 
-Incorporating learnings from Google tutorial on [managing infrastructure as code](https://cloud.google.com/solutions/managing-infrastructure-as-code) to use Cloud Build and Terraform. 
+Incorporating learnings from Google tutorial on [managing infrastructure as code](https://cloud.google.com/solutions/managing-infrastructure-as-code) to use Cloud Build and Terraform.
+
+
+## Flow of logic:
+1. pushing a feature branch to GitHub triggers the following:
+  - root directory `cloudbuild.yaml` Build
+  <!-- - flask app directory `Dockerfile` build  -->
+
+2. Terraform builds a plan which includes the plan to create the Cloud Build Trigger
+3. We MERGE TO DEV
+4. TF rebuilds the plan and then applies the plan
+4. The Cloud Build trigger is created in DEV, and the Dockerfile built and the image placed in Artifact Registry
+4. Cloud Run is launched via the Docker image created by Cloud Build
+5. _We make changes to a flask file in a new branch, and push to GitHub
+6.
--- environments/dev/flask_app.tf
+++ environments/dev/flask_app.tf
@@ -12,90 +12,10 @@ resource "google_cloudbuild_trigger" "dev-flask-app-filename-build-trigger" {
   included_files  = ["flask_app_cloud_run/**"]
   filename        = "flask_app_cloud_run/cloudbuild.yaml"
 
-  # build {
-  #   images = ["europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA"]
-  #   step {
-  #     name        = "gcr.io/cloud-builders/docker"
-  #     args        = ["build", "-f", "./flask_app_cloud_run/Dockerfile", "-t", "europe-west1-docker.pkg.dev/$PROJECT_ID/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA", "./flask_app_cloud_run" ]
-  #     id          = "build & push flask_app_cloud_run image"
-  #   }
-  # }
 }
 
-# resource "google_endpoints_service" "openapi_service" {
-#   service_name   = "api-name.endpoints.project-id.cloud.goog"
-#   project        = var.project_id
-#   openapi_config = file("openapi_spec.yml")
-# }
-
-# resource "google_cloud_run_service" "dev-flask-app-cloud-run" {
-#   name      = "dev-flask-app-cloud-run-service"
-#   location  = "europe-west1"
-#   project = var.project_id
-#
-#   template {
-#     spec {
-#       container_concurrency = 80
-#       timeout_seconds       = 300
-#       containers {
-#         image = "europe-west1-docker.pkg.dev/${var.project_id}/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA"
-#       }
-#     }
-#   }
-#
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-#
-#   depends_on = [google_cloudbuild_trigger.dev-flask-app-build-trigger]
-# }
-
-# data "google_iam_policy" "noauth" {
-#   binding {
-#     role = "roles/run.invoker"
-#     members = [
-#       "allUsers",
-#     ]
-#   }
-# }
-#
-# resource "google_cloud_run_service_iam_policy" "noauth" {
-#   location    = google_cloud_run_service.dev-flask-app-cloud-run.location
-#   project     = google_cloud_run_service.dev-flask-app-cloud-run.project
-#   service     = google_cloud_run_service.dev-flask-app-cloud-run.name
-#
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
-
-
-# resource "null_resource" "dev-deploy-latest-cloud-run-revision" {
-#   depends_on = [google_cloudbuild_trigger.dev-flask-app-build-trigger]
-#
-#   provisioner "local-exec" {
-#     command = <<EOF
-#     "gcloud run deploy dev-flask-app-cloud-run-service \
-#            --image=europe-west1-docker.pkg.dev/${var.project_id}/dev-gcp-cloud-run-flask-app-example/flask-endpoint-image:$SHORT_SHA,
-#            --region europe-west1,
-#            --platform managed',
-#            --allow-unauthenticated"
-# EOF
-#
-#   }
-# }
-
-
-# module "cli" {
-#   source = "../.."
-#
-#   platform              = "linux"
-#   additional_components = ["kubectl", "beta"]
-#
-#   create_cmd_body  = "services enable youtube.googleapis.com --project ${var.project_id}"
-#   destroy_cmd_body = "services disable youtube.googleapis.com --project ${var.project_id}"
-#
-#   module_depends_on = [
-#     module.hello.wait,
-#     module.two.wait
-#   ]
-# }
+resource "google_endpoints_service" "openapi_service" {
+  service_name   = "api-name.endpoints.project-id.cloud.goog"
+  project        = var.project_id
+  openapi_config = file("openapi_spec.yml")
+}
